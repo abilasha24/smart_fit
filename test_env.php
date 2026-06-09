f9df2299ada4d78dc36a7620401306f5cfcf4370
abilasha24 <selvanayagamabilasha@gmail.com>
2026-06-09 23:16:16 +0530

    fix: update test_env with all vars

--- test_env.php
+++ test_env.php
@@ -1,2 +1,10 @@
 <?php
-echo getenv('STRIPE_SECRET_KEY');
+header('Content-Type: application/json');
+echo json_encode([
+  "MYSQLHOST"     => getenv('MYSQLHOST'),
+  "MYSQLUSER"     => getenv('MYSQLUSER'),
+  "MYSQLPASSWORD" => getenv('MYSQLPASSWORD') ? "***SET***" : "NOT SET",
+  "MYSQLDATABASE" => getenv('MYSQLDATABASE'),
+  "MYSQLPORT"     => getenv('MYSQLPORT'),
+  "STRIPE_KEY"    => getenv('STRIPE_SECRET_KEY') ? "***SET***" : "NOT SET",
+], JSON_PRETTY_PRINT);
